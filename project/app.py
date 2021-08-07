845b027e30249bffcf8c9b7cc48d289e2e1c1b1e
Boris Kazarski <yonic.bk@gmail.com>
2021-08-08 00:13:56 +0300

    Projects formatted

--- project/app.py
+++ project/app.py
@@ -4,19 +4,28 @@ from pathlib import Path
 from functools import wraps
 from typing import Union
 
-from flask import Flask, render_template, request, session, redirect, flash, url_for, jsonify, Response
+from flask import (
+    Flask,
+    render_template,
+    request,
+    session,
+    redirect,
+    flash,
+    url_for,
+    jsonify,
+    Response,
+)
 from flask_sqlalchemy import SQLAlchemy
 
 basedir = Path(__file__).resolve().parent.parent
 
 # configuration
-DATABASE = 'flaskr.db'
-USERNAME = 'admin'
-PASSWORD = 'admin'
-SECRET_KEY = 'change_me'
+DATABASE = "flaskr.db"
+USERNAME = "admin"
+PASSWORD = "admin"
+SECRET_KEY = "change_me"
 SQLALCHEMY_DATABASE_URI = os.getenv(
-    'DATABASE_URL',
-    f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
+    "DATABASE_URL", f"sqlite:///{Path(basedir).joinpath(DATABASE)}"
 )
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -37,80 +46,83 @@ class Status(Enum):
     Success = 1
 
 
-@app.route('/')
+@app.route("/")
 def index():
     """Searches the database for entries, then displays them."""
     entries = db.session.query(models.Post)
-    return render_template('index.html', entries=entries)
+    return render_template("index.html", entries=entries)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login() -> Union[Response, str]:
     """User login/authentication/session management."""
     error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
+    if request.method == "POST":
+        if request.form["username"] != app.config["USERNAME"]:
+            error = "Invalid username"
+        elif request.form["password"] != app.config["PASSWORD"]:
+            error = "Invalid password"
         else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('index'))
-    return render_template('login.html', error=error)
+            session["logged_in"] = True
+            flash("You were logged in")
+            return redirect(url_for("index"))
+    return render_template("login.html", error=error)
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout() -> Response:
     """User logout/authentication/session management"""
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('index'))
+    session.pop("logged_in", None)
+    flash("You were logged out")
+    return redirect(url_for("index"))
 
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if not session.get('logged_in'):
-            flash('Please log in')
-            return jsonify({'status': Status.Failure.value, 'message': 'Please log in'}), 401
+        if not session.get("logged_in"):
+            flash("Please log in")
+            return (
+                jsonify({"status": Status.Failure.value, "message": "Please log in"}),
+                401,
+            )
         return f(*args, **kwargs)
 
     return decorated_function
 
 
-@app.route('/add', methods=['POST'])
+@app.route("/add", methods=["POST"])
 @login_required
 def add_entry() -> Response:
     """Adds a new post to the database."""
-    new_entry = models.Post(request.form['title'], request.form['text'])
+    new_entry = models.Post(request.form["title"], request.form["text"])
     db.session.add(new_entry)
     db.session.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('index'))
+    flash("New entry was successfully posted")
+    return redirect(url_for("index"))
 
 
-@app.route('/delete/<post_id>', methods=['GET'])
+@app.route("/delete/<post_id>", methods=["GET"])
 @login_required
 def delete_entry(post_id: int) -> Response:
     """Deletes a post from the database"""
     try:
         db.session.query(models.Post).filter_by(id=post_id).delete()
         db.session.commit()
-        result = {'status': Status.Success.value, 'message': 'Post Deleted'}
+        result = {"status": Status.Success.value, "message": "Post Deleted"}
     except Exception as e:
-        result = {'status': Status.Failure.value, 'message': repr(e)}
+        result = {"status": Status.Failure.value, "message": repr(e)}
     return jsonify(result)
 
 
-@app.route('/search', methods=['GET'])
+@app.route("/search", methods=["GET"])
 def search() -> str:
-    query = request.args.get('query')
+    query = request.args.get("query")
     entries = db.session.query(models.Post)
     if query:
-        return render_template('search.html', entries=entries, query=query)
-    return render_template('search.html')
+        return render_template("search.html", entries=entries, query=query)
+    return render_template("search.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
--- tests/app_test.py
+++ tests/app_test.py
@@ -3,15 +3,15 @@ import pytest
 from pathlib import Path
 from project.app import app, db, Status
 
-TEST_DB = 'test.db'
+TEST_DB = "test.db"
 
 
 @pytest.fixture
 def client():
     BASE_DIR = Path(__file__).resolve().parent.parent
-    app.config['TESTING'] = True
-    app.config['DATABASE'] = BASE_DIR.joinpath(TEST_DB)
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{BASE_DIR.joinpath(TEST_DB)}'
+    app.config["TESTING"] = True
+    app.config["DATABASE"] = BASE_DIR.joinpath(TEST_DB)
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{BASE_DIR.joinpath(TEST_DB)}"
 
     db.create_all()  # setup
     yield app.test_client()  # tests run here
@@ -21,7 +21,7 @@ def client():
 def login(client, username, password):
     """Login helper function"""
     return client.post(
-        '/login',
+        "/login",
         data=dict(username=username, password=password),
         follow_redirects=True,
     )
@@ -29,82 +29,78 @@ def login(client, username, password):
 
 def logout(client):
     """Logout helper function"""
-    return client.get('/logout', follow_redirects=True)
+    return client.get("/logout", follow_redirects=True)
 
 
 def search(client, query):
-    return client.get(
-        '/search',
-        query_string=dict(query=query),
-        follow_redirects=True
-    )
+    return client.get("/search", query_string=dict(query=query), follow_redirects=True)
 
 
 def add_entry(client, title, text):
     return client.post(
-        '/add',
+        "/add",
         data=dict(title=title, text=text),
         follow_redirects=True,
     )
 
 
 def test_index(client):
-    response = client.get('/', content_type='html/text')
+    response = client.get("/", content_type="html/text")
     assert response.status_code == 200
 
 
 def test_database(client):
     """Initial test, ensure that the database exists"""
-    assert Path('flaskr.db').is_file()
+    assert Path("flaskr.db").is_file()
 
 
 def test_empty_db(client):
     """Ensure database is blank"""
-    rv = client.get('/')
-    assert b'No entries yet. Add some!' in rv.data
+    rv = client.get("/")
+    assert b"No entries yet. Add some!" in rv.data
 
 
 def test_login_logout(client):
     """Test login and logout using helper functions"""
-    rv = login(client, app.config['USERNAME'], app.config['PASSWORD'])
-    assert b'You were logged in' in rv.data
+    rv = login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    assert b"You were logged in" in rv.data
     rv = logout(client)
-    assert b'You were logged out' in rv.data
-    rv = login(client, app.config['USERNAME'] + 'x', app.config['PASSWORD'])
-    assert b'Invalid username' in rv.data
-    rv = login(client, app.config['USERNAME'], app.config['PASSWORD'] + 'x')
-    assert b'Invalid password' in rv.data
+    assert b"You were logged out" in rv.data
+    rv = login(client, app.config["USERNAME"] + "x", app.config["PASSWORD"])
+    assert b"Invalid username" in rv.data
+    rv = login(client, app.config["USERNAME"], app.config["PASSWORD"] + "x")
+    assert b"Invalid password" in rv.data
 
 
 def test_messages(client):
     """Ensure that user can post messages"""
-    login(client, app.config['USERNAME'], app.config['PASSWORD'])
-    rv = add_entry(client, '<Hello>', '<strong>HTML</strong> allowed here')
-    assert b'No entries here so far' not in rv.data
-    assert b'&lt;Hello&gt;' in rv.data
-    assert b'<strong>HTML</strong> allowed here' in rv.data
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = add_entry(client, "<Hello>", "<strong>HTML</strong> allowed here")
+    assert b"No entries here so far" not in rv.data
+    assert b"&lt;Hello&gt;" in rv.data
+    assert b"<strong>HTML</strong> allowed here" in rv.data
 
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
     # Delete fails before login.
-    rv = client.get('/delete/1')
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
-    assert data['status'] == Status.Failure.value
+    assert data["status"] == Status.Failure.value
 
     # Delete succeeds after login.
-    login(client, app.config['USERNAME'], app.config['PASSWORD'])
-    rv = client.get('/delete/1')
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
-    assert data['status'] == Status.Success.value
+    assert data["status"] == Status.Success.value
 
 
 def test_search(client):
     """Ensure that the search returns the correct entries"""
-    login(client, app.config['USERNAME'], app.config['PASSWORD'])
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
 
-    test_titles = ['title1', 'title2', 'title3']
-    test_texts = ['text1', 'text2', 'text3']
+    test_titles = ["title1", "title2", "title3"]
+    test_texts = ["text1", "text2", "text3"]
 
     # Init the db with some entries
     for title, text in zip(test_titles, test_texts):
@@ -116,22 +112,48 @@ def test_search(client):
 
         for title, text in zip(test_titles, test_texts):
             if title in allowed_titles or text in allowed_texts:
-                assert bytes(title, 'utf-8') in html_content and bytes(text, 'utf-8') in html_content
+                assert (
+                    bytes(title, "utf-8") in html_content
+                    and bytes(text, "utf-8") in html_content
+                )
             else:
-                assert bytes(title, 'utf-8') not in html_content and bytes(text, 'utf-8') not in html_content
+                assert (
+                    bytes(title, "utf-8") not in html_content
+                    and bytes(text, "utf-8") not in html_content
+                )
 
     # Test empty query
-    assert_successful_search(search(client, query='').data, allowed_titles=[], allowed_texts=[])
+    assert_successful_search(
+        search(client, query="").data, allowed_titles=[], allowed_texts=[]
+    )
 
     # Test that all title and text entries match
-    assert_successful_search(search(client, query='title').data, allowed_titles=test_titles, allowed_texts=test_texts)
-    assert_successful_search(search(client, query='text').data, allowed_titles=test_titles, allowed_texts=test_texts)
+    assert_successful_search(
+        search(client, query="title").data,
+        allowed_titles=test_titles,
+        allowed_texts=test_texts,
+    )
+    assert_successful_search(
+        search(client, query="text").data,
+        allowed_titles=test_titles,
+        allowed_texts=test_texts,
+    )
 
     # Test exact text and title match
-    assert_successful_search(search(client, query=test_titles[0]).data, allowed_titles=test_titles[:1],
-                             allowed_texts=test_texts[:1])
-    assert_successful_search(search(client, query=test_texts[0]).data, allowed_titles=test_titles[:1],
-                             allowed_texts=test_texts[:1])
+    assert_successful_search(
+        search(client, query=test_titles[0]).data,
+        allowed_titles=test_titles[:1],
+        allowed_texts=test_texts[:1],
+    )
+    assert_successful_search(
+        search(client, query=test_texts[0]).data,
+        allowed_titles=test_titles[:1],
+        allowed_texts=test_texts[:1],
+    )
 
     # Test subpattern match
-    assert_successful_search(search(client, query='3').data, allowed_titles=test_titles[-1:], allowed_texts=test_texts[-1:])
+    assert_successful_search(
+        search(client, query="3").data,
+        allowed_titles=test_titles[-1:],
+        allowed_texts=test_texts[-1:],
+    )
